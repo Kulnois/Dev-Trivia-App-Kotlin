e82ee4c20f96c1dcf068533dd2c0160a41e09c13
Kulnois <rasecmen58@hotmail.com>
2020-08-23 09:11:44 -0500

    Init project

---
+++ app/src/main/java/com/kulnois/triviadev/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kulnois.triviadev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
